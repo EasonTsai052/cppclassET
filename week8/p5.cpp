cc007fbe09086bf58bc6b6ebad754d0fe95dccb4
ET01 <EasonTsai0502@gmail.com>
2021-04-24 12:01:20 +0800

    [main][1.0.2][2021-0424-12:00]
     [main][1.0.2][2021-0424-12:00]{
        [add][files](
          [.cpp ]["/week8/p5.cpp"]
        )
        [edt][files]{
          ["/week8/p5"]{
            "practice"
          }
        }

---
+++ week8/p5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	int lista[a]={};
+	for(int fo_a=0;fo_a<a;fo_a++){
+		cin>>lista[fo_a];
+	}
+	return 0;
+}
--- week8/sort2.cpp
+++ week8/sort2.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    for(int fo_a=0;fo_a<a;fo_a++){
+        int b;
+        cin>>b;
+    }
+}
